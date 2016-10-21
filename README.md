286c1cdf6f05bb7acaf264713882e4f4a409a96f
Brian Trammell <brian@trammell.ch>
2016-10-21 13:48:32 +0200

    wip

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ This repository contains a reference implementation of a RAINS server,
 supporting authority, intermediary, and query service, as well as associated
 tools for managing the server. It has the following entry points:
 
-- `rainsd`:    A RAINS server
-- `rainsdig`:  A command-line RAINS client for query debugging
-- `rainspub`:  A command-line RAINS authority client for 
-               publishing assertions to an authority service
+- `rainsd`:   A RAINS server
+- `rainsdig`: A command-line RAINS client for query debugging
+- `rainspub`: A command-line RAINS authority client for 
+              publishing assertions to an authority service
+- `rainsfic`: A tool for generating assertions to publish for
+              publishing with `rainspub` for testing purposes.
 
-In addition, the `rainslib` library on which the server and tools are built provides common handling for the CBOR-based RAINS information model.
+In addition, the `rainslib` library on which the server and tools are built
+provides common handling for the CBOR-based RAINS information model.
--- model.go
+++ model.go
@@ -8,46 +8,46 @@ import (
 type mapKey uint
 
 const (
-	content      mapKey = 0  // Content of a message, shard, or zone
-	capabilities mapKey = 1  // Capabilities of server sending message
-	signatures   mapKey = 2  // Signatures on a message or section
-	subject_name mapKey = 3  // Subject name in an assertion
-	subject_zone mapKey = 4  // Zone name in an assertion
-	query_name   mapKey = 5  // Qualified subject name in a query
-	context      mapKey = 6  // Context of an assertion
-	objects      mapKey = 7  // Objects of an assertion
-	token        mapKey = 8  // Token for referring to a data item
-	shard_range  mapKey = 11 // Lexical range of Assertions in Shard
-	query_types  mapKey = 14 // acceptable object types for query
-	note_type    mapKey = 17 // Notification type
-	query_opts   mapKey = 22 // Set of query options requested
-	note_data    mapKey = 23 // Additional notification data
+	mk_content      mapKey = 0  // Content of a message, shard, or zone
+	mk_capabilities mapKey = 1  // Capabilities of server sending message
+	mk_signatures   mapKey = 2  // Signatures on a message or section
+	mk_subject_name mapKey = 3  // Subject name in an assertion
+	mk_subject_zone mapKey = 4  // Zone name in an assertion
+	mk_query_name   mapKey = 5  // Qualified subject name in a query
+	mk_context      mapKey = 6  // Context of an assertion
+	mk_objects      mapKey = 7  // Objects of an assertion
+	mk_token        mapKey = 8  // Token for referring to a data item
+	mk_shard_range  mapKey = 11 // Lexical range of Assertions in Shard
+	mk_query_types  mapKey = 14 // acceptable object types for query
+	mk_note_type    mapKey = 17 // Notification type
+	mk_query_opts   mapKey = 22 // Set of query options requested
+	mk_note_data    mapKey = 23 // Additional notification data
 )
 
 type sectionKey uint
 
 const (
-	assertion    sectionKey = 1  // Assertion (see Section 5.4)
-	shard        sectionKey = 2  // Shard (see Section 5.5)
-	zone         sectionKey = 3  // Zone (see Section 5.6)
-	query        sectionKey = 4  // Query (see Section 5.7)
-	notification sectionKey = 23 // Notification (see Section 5.8)
+	sk_assertion    sectionKey = 1  // Assertion (see Section 5.4)
+	sk_shard        sectionKey = 2  // Shard (see Section 5.5)
+	sk_zone         sectionKey = 3  // Zone (see Section 5.6)
+	sk_query        sectionKey = 4  // Query (see Section 5.7)
+	sk_notification sectionKey = 23 // Notification (see Section 5.8)
 )
 
 type objectType uint
 
 const (
-	name         objectType = 1  // name associated with subject
-	ip6_addr     objectType = 2  // IPv6 address of subject
-	ip4_addr     objectType = 3  // IPv4 address of subject
-	redirection  objectType = 4  // name of zone authority server
-	delegation   objectType = 5  // public key for zone delgation
-	nameset      objectType = 6  // name set expression for zone
-	cert_info    objectType = 7  // certificate information for name
-	service_info objectType = 8  // service information for srvname
-	registrar    objectType = 9  // registrar information
-	registrant   objectType = 10 // registrant information
-	infrakey     objectType = 11 // public key for RAINS infrastructure
+	ot_name         objectType = 1  // name associated with subject
+	ot_ip6_addr     objectType = 2  // IPv6 address of subject
+	ot_ip4_addr     objectType = 3  // IPv4 address of subject
+	ot_redirection  objectType = 4  // name of zone authority server
+	ot_delegation   objectType = 5  // public key for zone delgation
+	ot_nameset      objectType = 6  // name set expression for zone
+	ot_cert_info    objectType = 7  // certificate information for name
+	ot_service_info objectType = 8  // service information for srvname
+	ot_registrar    objectType = 9  // registrar information
+	ot_registrant   objectType = 10 // registrant information
+	ot_infrakey     objectType = 11 // public key for RAINS infrastructure
 )
 
 type algorithmType uint
@@ -65,29 +65,139 @@ type Signature struct {
 	content         []byte
 }
 
-// FIXME: is this an interface? go read the go book...
-type Object struct {
+type Object interface {
+	sliceContents() []interface{}
+}
+
+type NameObject string
+
+func (this *NameObject) sliceContents() []interface{} {
+	out := make([]interface{}, 2)
+	out[0] = ot_name
+	out[1] = *this
+	return out
+}
+
+type IP6AddrObject [16]byte
+
+func (this *IP6AddrObject) sliceContents() []interface{} {
+	out := make([]interface{}, 2)
+	out[0] = ot_ip6_addr
+	out[1] = *this
+	return out
+}
+
+type IP4AddrObject [4]byte
+
+func (this *IP4AddrObject) sliceContents() []interface{} {
+	out := make([]interface{}, 2)
+	out[0] = ot_ip4_addr
+	out[1] = *this
+	return out
+}
+
+type RedirectionObject string
+
+func (this *RedirectionObject) sliceContents() []interface{} {
+	out := make([]interface{}, 2)
+	out[0] = ot_redirection
+	out[1] = *this
+	return out
+}
+
+type DelegationObject struct {
+	Alg     algorithmType
+	Content []byte
+}
+
+func (this *DelegationObject) sliceContents() []interface{} {
+	out := make([]interface{}, 3)
+	out[0] = ot_delegation
+	out[1] = this.Alg
+	out[2] = this.Content
+	return out
+}
+
+type NamesetObject string
+
+func (this *NamesetObject) sliceContents() []interface{} {
+	out := make([]interface{}, 2)
+	out[0] = ot_nameset
+	out[1] = *this
+	return out
+}
+
+// type CertificateObject struct {
+// }
+
+type ServiceObject struct {
+	Hostname      string
+	TransportPort uint16
+	Priority      uint16
+}
+
+func (this *ServiceObject) sliceContents() []interface{} {
+	out := make([]interface{}, 4)
+	out[0] = ot_service_info
+	out[1] = this.Hostname
+	out[2] = this.TransportPort
+	out[3] = this.Priority
+	return out
+}
+
+type RegistrarObject string
+
+func (this *RegistrarObject) sliceContents() []interface{} {
+	out := make([]interface{}, 4)
+	out[0] = ot_registrar
+	out[1] = *this
+	return out
+}
+
+type RegistrantObject string
+
+func (this *RegistrantObject) sliceContents() []interface{} {
+	out := make([]interface{}, 4)
+	out[0] = ot_registrar
+	out[1] = *this
+	return out
+}
+
+type InfrakeyObject struct {
+	Alg     algorithmType
+	Content []byte
+}
+
+func (this *InfrakeyObject) sliceContents() []interface{} {
+	out := make([]interface{}, 3)
+	out[0] = ot_infrakey
+	out[1] = this.Alg
+	out[2] = this.Content
+	return out
 }
 
 type Assertion struct {
-	subjectName string
-	subjectZone string
-	context     string
-	objects     []Object
-	signatures  []Signature
+	Name       string
+	Zone       string
+	Context    string
+	Objects    []Object
+	Signatures []Signature
 }
 
 type AssertionSet struct {
-	subjectZone  string
-	context      string
-	assertions   []Assertion
-	signatures   []Signature
-	shardTange   []string
-	zoneComplete bool
+	Zone         string
+	Context      string
+	Assertions   []Assertion
+	Signatures   []Signature
+	ShardRange   []string
+	ZoneComplete bool
 }
 
 type Query struct {
-	name        string
-	contexts    []string
-	objectTypes []objectType
+	Name        string
+	Contexts    []string
+	ObjectTypes []objectType
+}
+
+type Notification struct {
 }
