d33d1781d37aec061d738ac75ac2484d77012a75
Brian Trammell <trammell@tik.ee.ethz.ch>
2016-10-25 09:58:58 +0200

    separate out switchboard

--- switchboard.go
+++ switchboard.go
@@ -1,4 +1,4 @@
-package main
+package rains
 
 import (
 	"bufio"
@@ -6,30 +6,32 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 const (
-	RAINSD_PORT = 1228
-	OUTBOX_LEN  = 20
-	INBOX_LEN   = 20
+	OUTBOX_LEN = 20
+	INBOX_LEN  = 20
 )
+
+// Switchboard manages connections to peers
 
 type Switchboard struct {
 	outboxes  map[string]chan string
 	inbox     chan string
-	localName string
+	LocalName string
 }
 
-func writeOutbox(w io.Writer, peerName string, outbox <-chan string) {
+func writeOutbox(w io.WriteCloser, peerName string, outbox <-chan string) {
 	for msg := range outbox {
-		fmt.Fprintln(w, string)
+		fmt.Fprintln(w, msg)
 	}
 
 	w.Close()
 }
 
 func readInbox(r io.Reader, peerName string, inbox chan<- string) {
-	scanner = bufio.NewScanner(r)
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		inbox <- scanner.Text()
 	}
@@ -37,26 +39,29 @@ func readInbox(r io.Reader, peerName string, inbox chan<- string) {
 }
 
 func (sb *Switchboard) setupConnection(peerName string, conn net.Conn) {
-	outbox = make(chan []byte, OUTQ_LEN)
+	outbox := make(chan string, OUTBOX_LEN)
 	sb.outboxes[peerName] = outbox
 	go writeOutbox(conn, peerName, outbox)
 	go readInbox(conn, peerName, sb.inbox)
 }
 
-func (sb *Switchboard) sendMessage(peerName string, message string) error {
-	var outbox chan []byte
+// Send a message to a peer identified by name.
+// Open a connection to that peer if none exists.
+func (sb *Switchboard) SendMessage(peerName string, address string, message string) error {
+	var outbox chan string
 	var ok bool
 
 	outbox, ok = sb.outboxes[peerName]
 
 	if !ok {
 		// open a connection and start handlers on it
-		if conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", address, RAINSD_PORT)); err != nil {
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
 			return err
 		}
 
 		sb.setupConnection(peerName, conn)
-		outbox <- "HELLO " + localName
+		outbox <- "HELLO " + sb.LocalName
 		log.Printf("switchboard opened connection to %v at %v\n", peerName, conn.RemoteAddr)
 
 	}
@@ -65,23 +70,30 @@ func (sb *Switchboard) sendMessage(peerName string, message string) error {
 	return nil
 }
 
-func (sb *Switchboard) listen() error {
-	if listener, err := net.Listen("tcp", fmt.Sprintf("*:%d", RAINSD_PORT)); err != nil {
-		return error
+func (sb *Switchboard) NextMessage() string {
+	return <-sb.inbox
+}
+
+func (sb *Switchboard) Listen(port int) error {
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("*:%d", port))
+	if err != nil {
+		return err
 	}
 
 	for {
-		if conn, err := listener.Accept(); err != nil {
+		conn, err := listener.Accept()
+		if err != nil {
 			log.Print(err)
 			continue
 		}
 
 		go func() {
-			scanner = bufio.NewScanner(conn)
+			scanner := bufio.NewScanner(conn)
 			if scanner.Scan() {
 				hello := scanner.Text()
 				peerName := strings.Split(hello, " ")[1]
-				sb.setup(peerName, conn)
+				sb.setupConnection(peerName, conn)
 				log.Printf("switchboard got connection from %v at %v\n", peerName, conn.RemoteAddr)
 			}
 		}()
@@ -89,17 +101,16 @@ func (sb *Switchboard) listen() error {
 
 }
 
+func NewSwitchboard() *Switchboard {
+	var sb Switchboard
+	sb.inbox = make(chan string, INBOX_LEN)
+	return &sb
+}
+
 func (sb *Switchboard) reapConnection(peerName string) {
 	outbox, ok := sb.outboxes[peerName]
 	if ok {
 		delete(sb.outboxes, peerName)
-		outbox.Close()
+		close(outbox)
 	}
-}
-
-func main() {
-
-	sb := new(Switchboard)
-	go sb.listen()
-
 }
